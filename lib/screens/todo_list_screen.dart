b821e2843378ec0e92c7b196965a800e92029dfd
Thaanu Perera <thaanup@gmail.com>
2021-12-27 14:16:00 +0530

    Add todo list screen UI

--- lib/screens/todo_list_screen.dart
+++ lib/screens/todo_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({Key? key}) : super(key: key);
@@ -11,7 +12,59 @@ class _TodoListScreenState extends State<TodoListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      backgroundColor: const Color(0xFFF2F1F6),
+      floatingActionButton: FloatingActionButton.extended(
+        label: const Text('New Task'),
+        icon: const Icon(Icons.add_circle_sharp),
+        onPressed: () {},
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(
+            30, MediaQuery.of(context).padding.top + 10, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              (DateFormat('EEEE dd LLLL')
+                  .format(DateTime.now())
+                  .toString()
+                  .toUpperCase()),
+              style: TextStyle(
+                fontWeight: FontWeight.w600,
+                color: Colors.grey[700],
+              ),
+            ),
+            const SizedBox(height: 5),
+            const Text(
+              'Task List',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(top: 15, bottom: 15),
+              padding: const EdgeInsets.all(10),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    'Todo',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }

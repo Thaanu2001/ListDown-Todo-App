df663fbed8baf6347b0604269d8d6168fda03994
Thaanu Perera <thaanup@gmail.com>
2021-12-27 23:07:23 +0530

    Fix font scale and status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:listdown_todo_app/screens/todo_list_screen.dart';
 
 void main() {
@@ -11,11 +12,26 @@ class ListDown extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(
+          statusBarColor: Colors.transparent, // Color for Android
+          statusBarBrightness:
+              Brightness.light // Dark == white status bar -- for IOS.
+          ),
+    );
+
     return MaterialApp(
       title: 'ListDown',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      builder: (BuildContext context, Widget? child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+          child: child as Widget,
+        );
+      },
       home: const TodoListScreen(),
     );
   }
